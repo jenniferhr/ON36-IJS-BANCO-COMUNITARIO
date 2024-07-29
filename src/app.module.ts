ebd43490cd70cf30a04e145f11d8aef5dd1536de
Jennifer Carvalho <jenniferh7637@gmail.com>
2024-07-29 18:58:22 -0300

    remove importação de modulo contas

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,9 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ContasModule } from './contas/contas.module';
 
 @Module({
-  imports: [ContasModule],
+  imports: [],
   controllers: [AppController],
   providers: [AppService],
 })
