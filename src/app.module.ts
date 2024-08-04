9df34f1dca07bc0d4612502da60fee32f3080fc9
Jennifer Carvalho <jenniferh7637@gmail.com>
2024-08-04 16:24:21 -0300

    cria modulo contas vazio

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { GerenteModule } from './gerente/gerente.module';
 import { ClientesModule } from './clientes/clientes.module';
+import { ContasModule } from './contas/contas.module';
 
 @Module({
-  imports: [GerenteModule, ClientesModule],
+  imports: [GerenteModule, ClientesModule, ContasModule],
   controllers: [AppController],
   providers: [AppService],
 })
