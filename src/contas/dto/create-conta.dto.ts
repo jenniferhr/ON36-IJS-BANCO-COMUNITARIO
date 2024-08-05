fd857c86031395f4af902da7af624bd0e62fafa1
Jennifer Carvalho <jenniferh7637@gmail.com>
2024-08-04 22:25:41 -0300

    adiciona class validator e atualiza dto de criarconta

--- src/contas/dto/create-conta.dto.ts
+++ src/contas/dto/create-conta.dto.ts
@@ -1,1 +1,37 @@
-export class CreateContaDto {}
+import {
+  IsEnum,
+  IsNotEmpty,
+  IsNumber,
+  IsOptional,
+  ValidateIf,
+} from 'class-validator';
+import { TipoConta } from 'src/interfaces/IConta';
+
+TODO: 'Corrigir essa validação pra fazer ela funcionar';
+export class CreateContaDto {
+  @IsNotEmpty()
+  @IsNumber()
+  idCliente: number;
+
+  @IsNotEmpty()
+  @IsNumber()
+  idGerente: number;
+
+  @IsNotEmpty()
+  @IsNumber()
+  numeroConta: number;
+
+  @IsNotEmpty()
+  @IsEnum(TipoConta)
+  tipo: TipoConta;
+
+  @ValidateIf((o) => o.tipo === TipoConta.Poupanca)
+  @IsOptional()
+  @IsNumber()
+  taxaJuros?: number;
+
+  @ValidateIf((o) => o.tipo === TipoConta.Corrente)
+  @IsOptional()
+  @IsNumber()
+  limiteChequeEspecial?: number;
+}
