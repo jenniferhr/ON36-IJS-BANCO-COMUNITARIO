6ef426d3986bde0983e22a6bc9193a6461495dbc
Jennifer Carvalho <jenniferh7637@gmail.com>
2024-09-08 17:06:31 -0300

    adiciona teste de integração de gerente

---
+++ test/gerente.e2e-spec.ts
@@ -0,0 +1,55 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { INestApplication } from '@nestjs/common';
+import * as request from 'supertest';
+import { AppModule } from '../src/app.module';
+
+const criaGerente = {
+  nomeCompleto: 'Pedro Silva',
+};
+
+describe('GerenteService (integração)', () => {
+  let app: INestApplication;
+
+  beforeAll(async () => {
+    const moduleFixture: TestingModule = await Test.createTestingModule({
+      imports: [AppModule], // Importe o módulo principal ou os módulos relevantes
+    }).compile();
+
+    app = moduleFixture.createNestApplication();
+    await app.init();
+  });
+
+  afterAll(async () => {
+    await app.close();
+  });
+
+  it('Deve criar um novo gerente', async () => {
+    return request(app.getHttpServer())
+      .post('/gerente')
+      .send(criaGerente)
+      .expect(201)
+      .expect((res) => {
+        expect(res.body).toHaveProperty('nomeCompleto', 'Pedro Silva');
+      });
+  });
+
+  it('Deve buscar um gerente', async () => {
+    const gerenteCriado = await request(app.getHttpServer())
+      .post('/gerente')
+      .send(criaGerente)
+      .expect(201);
+
+    const idGerente = gerenteCriado.body.id;
+
+    return request(app.getHttpServer())
+      .get(`/gerente/${idGerente}`) // Endpoint com o ID do gerente
+      .expect(200) // Espera um status de sucesso
+      .then((response) => {
+        expect(response.body).toMatchObject({
+          id: idGerente,
+          nomeCompleto: criaGerente.nomeCompleto,
+          clientes: [],
+        });
+      });
+  });
+});
